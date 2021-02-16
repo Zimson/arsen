493dc1a1e12198d2fa5b0c7a881cfc5c24bbab64
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-02-16 15:17:13 +1000

    Socal refactored

---
+++ src/helpers/social.js
@@ -0,0 +1,34 @@
+module.exports = function(options) {
+  const name = options.hash.name;
+  const width = options.hash.width;
+  const height = options.hash.height;
+  const mods = options.hash.mods;
+  const root = options.data.root.root;
+  let cssClass = 'social';
+  let allMods = '';
+
+  if (mods !== 'undefined' && mods ) {
+    const modsList = mods.split(',');
+      for (let i = 0; i < modsList.length; i++) {
+        allMods = allMods + ' social--' + modsList[i].trim();
+      }
+  }
+
+  cssClass+= allMods;
+
+  const block = `
+    <div class="${cssClass}">
+        <span class="social__label">Мы в соц. сетях</span>
+        <a href="https://www.instagram.com/arsenal_pay/" class="social__item">
+            <img src="${root}/assets/img/svg/instagram.svg" width="48" height="38" alt="Instagram">
+        </a>
+        <a href="tg://resolve?domain=arsenalpay" class="social__item">
+            <img src="${root}/assets/img/svg/telegram.svg" width="48" height="38" alt="Telegram">
+        </a>
+        <a href="https://www.facebook.com/ArsenalPay.ru" class="social__item">
+            <img src="${root}/assets/img/svg/facebook.svg" width="48" height="38" alt="Facebook">
+        </a>
+  </div>`;
+
+  return block;
+}
