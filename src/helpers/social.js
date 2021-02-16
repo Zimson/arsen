31e92bf45b892575c12205710e4d6e4f99c3e5b8
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-02-16 19:32:23 +1000

    Root for img fixed

--- src/helpers/social.js
+++ src/helpers/social.js
@@ -20,13 +20,13 @@ module.exports = function(options) {
     <div class="${cssClass}">
         <span class="social__label">Мы в соц. сетях</span>
         <a href="https://www.instagram.com/arsenal_pay/" class="social__item">
-            <img src="${root}/assets/img/svg/instagram.svg" width="48" height="38" alt="Instagram">
+            <img src="${root}assets/img/svg/instagram.svg" width="48" height="38" alt="Instagram">
         </a>
         <a href="tg://resolve?domain=arsenalpay" class="social__item">
-            <img src="${root}/assets/img/svg/telegram.svg" width="48" height="38" alt="Telegram">
+            <img src="${root}assets/img/svg/telegram.svg" width="48" height="38" alt="Telegram">
         </a>
         <a href="https://www.facebook.com/ArsenalPay.ru" class="social__item">
-            <img src="${root}/assets/img/svg/facebook.svg" width="48" height="38" alt="Facebook">
+            <img src="${root}assets/img/svg/facebook.svg" width="48" height="38" alt="Facebook">
         </a>
   </div>`;
 
