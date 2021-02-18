a400d5283fe27a377e752e55c667b755f6b7814a
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-02-18 13:04:37 +1000

    Footer fixed

--- src/helpers/social.js
+++ src/helpers/social.js
@@ -18,14 +18,14 @@ module.exports = function(options) {
 
   const block = `
     <div class="${cssClass}">
-        <span class="social__label">Мы в соц сетях</span>
-        <a href="https://www.instagram.com/arsenal_pay/" class="social__item">
+        <span class="social__label">Мы в соцсетях</span>
+        <a href="https://www.instagram.com/arsenal_pay/" class="social__item" target="_blank">
             <img src="${root}assets/img/svg/instagram.svg" width="48" height="38" alt="Instagram">
         </a>
         <a href="tg://resolve?domain=arsenalpay" class="social__item">
             <img src="${root}assets/img/svg/telegram.svg" width="48" height="38" alt="Telegram">
         </a>
-        <a href="https://www.facebook.com/ArsenalPay.ru" class="social__item">
+        <a href="https://www.facebook.com/ArsenalPay.ru" class="social__item" target="_blank">
             <img src="${root}assets/img/svg/facebook.svg" width="48" height="38" alt="Facebook">
         </a>
   </div>`;
