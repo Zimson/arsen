bdfdabb5f10436b50d782164bd43bfdf54197280
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-04-20 15:43:58 +1000

    Text refactored

---
+++ src/helpers/alert.js
@@ -0,0 +1,34 @@
+module.exports = function(options) {
+  const text = options.hash.text;
+  const style = options.hash.style;
+  const mods = options.hash.mods;
+  const icon = options.hash.icon ? JSON.parse(options.hash.icon) : null;
+  const root = options.data.root.root;
+
+  function setMods(cls, mods) {
+    let cssClass = cls;
+    let allMods = '';
+
+    if (mods !== 'undefined' && mods ) {
+      const modsList = mods.split(',');
+      for (let i = 0; i < modsList.length; i++) {
+        allMods = allMods + ` ${cls}--` + modsList[i].trim();
+      }
+    }
+
+    cssClass+= allMods;
+
+    return cssClass;
+  }
+
+  const alertClass = setMods('alert', mods);
+
+  const alert = `<article class="${alertClass}" ${style ? `style="${style}"` : ``}>
+                  <svg  class="icon" width="48" height="38">
+                    <use xlink:href="${root}assets/img/symbol/sprite.svg#check" />
+                  </svg>
+                  <span class="alert__text">${text}</span>
+                </article>`;
+
+  return alert;
+}
