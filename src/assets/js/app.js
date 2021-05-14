5b558c54ab2d4633e7f58ba9281f0d3224308bcf
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-05-14 12:50:02 +1000

    Validation added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,9 +2,9 @@ import 'what-input';
 
 //polyfill
 import './utils/polyfill';
-import {createApp} from 'vue/dist/vue.esm-bundler';
-// import useVuelidate from '@vuelidate/core'
-// import { required, email } from '@vuelidate/validators'
+import {createApp, reactive} from 'vue/dist/vue.esm-bundler';
+import useVuelidate from '@vuelidate/core'
+import {required, minLength, maxLength, numeric} from '@vuelidate/validators'
 
 import Modal from './modules/Modal';
 import MobileNav from './modules/MobileNav';
@@ -80,6 +80,33 @@ const app = createApp({
 });
 
 app.component('modal', {
+  setup () {
+    const state = reactive({
+      firstNumbers: '',
+      lastNumbers: '',
+      sum: ''
+    });
+    const rules = {
+      firstNumbers: {
+        required,
+        numeric,
+        minLength: minLength(6)
+      },
+      lastNumbers: {
+        required,
+        numeric,
+        minLength: minLength(4)
+      },
+      sum: {
+        required,
+        numeric
+      }
+    }
+
+    const v$ = useVuelidate(rules, state)
+
+    return {name , v$ }
+  },
   props: {
     id: String,
     title: String,
@@ -90,9 +117,17 @@ app.component('modal', {
       isOpen: true
     }
   },
+  // validations () {
+  //   return {
+  //     firstNumbers: {required}, // Matches this.firstName
+  //   }
+  // },
   methods: {
     closeModal() {
       // this.isOpen = false;
+    },
+    submit(data) {
+      console.log('submit', data);
     }
   },
   updated(data) {
@@ -101,13 +136,38 @@ app.component('modal', {
   template: `
     <article class="modal" v-if="isOpen">
       <div class="modal__body" id="deleteAutoPaymentForm">
-        <button type="button" class="modal__close" @click="closeModal()">
+        <button type="button" class="modal__close" @click="closeModal($event)">
 <!--      {{{ icon name="cross" width="12" height="12" }}}-->
         </button>
         <h1 class="modal__title">{{title}}</h1>
-        <form >
-          <input type="text" :placeholder="placeholder" />
-          <input type="text" placeholder="Введите последние 4 цифры"/>
+        <form action="" method="" @submit.prevent="submit($event)" >
+          <input type="text" v-model="v$.firstNumbers.$model" :class="{ error: v$.firstNumbers.$invalid && v$.firstNumbers.$dirty}" @blur="v$.firstNumbers.$touch" placeholder="Первые 6 цифр" />
+          <div class="input-errors" v-if="v$.firstNumbers.required.$invalid && v$.firstNumbers.$dirty">
+            <div class="error-msg">{{ v$.firstNumbers.required.$message }}</div>
+          </div>
+          <div class="input-errors" v-else-if="v$.firstNumbers.numeric.$invalid && v$.firstNumbers.$dirty">
+            <div class="error-msg">{{ v$.firstNumbers.numeric.$message }}</div>
+          </div>
+          <div class="input-errors" v-else-if="v$.firstNumbers.minLength.$invalid && v$.firstNumbers.$dirty">
+            <div class="error-msg">{{ v$.firstNumbers.minLength.$message }}</div>
+          </div>
+          <input type="text" v-model="v$.lastNumbers.$model" :class="{ error: v$.lastNumbers.$invalid && v$.lastNumbers.$dirty}" @blur="v$.lastNumbers.$touch" placeholder="Последние 4 цифры"/>
+          <div class="input-errors" v-if="v$.lastNumbers.required.$invalid && v$.lastNumbers.$dirty">
+            <div class="error-msg">{{ v$.lastNumbers.required.$message }}</div>
+          </div>
+          <div class="input-errors" v-else-if="v$.lastNumbers.numeric.$invalid && v$.lastNumbers.$dirty">
+            <div class="error-msg">{{ v$.lastNumbers.numeric.$message }}</div>
+          </div>
+          <div class="input-errors" v-else-if="v$.lastNumbers.minLength.$invalid && v$.lastNumbers.$dirty">
+            <div class="error-msg">{{ v$.lastNumbers.minLength.$message }}</div>
+          </div>
+          <input type="text" v-model="v$.sum.$model" :class="{ error: v$.sum.$invalid && v$.sum.$dirty}" @blur="v$.sum.$touch" placeholder="Сумма"/>
+          <div class="input-errors" v-if="v$.sum.required.$invalid && v$.sum.$dirty">
+            <div class="error-msg">{{ v$.sum.required.$message }}</div>
+          </div>
+          <div class="input-errors" v-else-if="v$.sum.numeric.$invalid && v$.sum.$dirty">
+            <div class="error-msg">{{ v$.sum.numeric.$message }}</div>
+          </div>
           <button type="submit">Найти автоплатеж</button>
         </form>
       </div>
