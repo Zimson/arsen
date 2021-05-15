cafd35afab68dc3ec2fe7d15cc63ab74951d8483
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-05-16 07:05:58 +1000

    Result-box added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -85,7 +85,48 @@ const app = createApp({
   }
 });
 
-app.use(VCalendar, {})
+app.use(VCalendar, {});
+
+app.component('app-alert', {
+  // props: {
+  //   'type': 'success'
+  // },
+  data() {
+    return {
+      // type: this.$props.type
+    }
+  },
+  template: `
+    <article class="alert" :class="'alert--'">
+      <svg  class="icon" width="48" height="38">
+        <use xlink:href="assets/img/symbol/sprite.svg#alert-success" />
+      </svg>
+      <span class="alert__text">Alert</span>
+    </article>
+  `
+});
+
+app.component('app-result-box', {
+  // props: {
+  //   'type': 'success'
+  // },
+  data() {
+    return {
+      // type: this.$props.type
+    }
+  },
+  template: `
+    <article class="result-box" >
+      <dl class="result-box__main">
+        <dt>Сайт</dt>
+        <dd>inetvl.ru</dd>
+        <dt>Идентификатор</dt>
+        <dd>12345678</dd>
+      </dl>
+      <button type="button" class="result-box__btn">Удалить</button>
+    </article>
+  `
+});
 
 app.component('app-modal', {
   setup () {
@@ -119,11 +160,16 @@ app.component('app-modal', {
     id: String,
     isOpen: Boolean,
   },
-  // data() {
-  //   return {
-  //     isOpen: true
-  //   }
-  // },
+  data() {
+    return {
+      titleMap: {
+        payments: 'Узнать о своем платеже',
+        findCheck: 'Найти свой чек',
+        deleteAutoPayment: 'Удалить автоплатеж',
+        deleteSavedCard: 'Удалить сохраненную карту'
+      }
+    }
+  },
   methods: {
     closeModal() {
       this.$emit('close', false);
@@ -133,8 +179,12 @@ app.component('app-modal', {
     }
   },
   mounted(data) {
-
-
+    // this.titleMap = {
+    //   payments: 'Узнать о своем платеже',
+    //   findCheck: 'Найти свой чек',
+    //   deleteAutoPayment: 'Удалить автоплатеж',
+    //   deleteSavedCard: 'Удалить сохраненную карту'
+    // }
   },
   template: `
     <article class="modal">
@@ -142,46 +192,61 @@ app.component('app-modal', {
         <button type="button" class="modal__close" @click="closeModal($event)">
 <!--      {{{ icon name="cross" width="12" height="12" }}}-->
         </button>
-        <h1 class="modal__title">{{id}}</h1>
-        <form action="" method="" @submit.prevent="submit($event)" >
-          <input type="text" v-model="v$.firstNumbers.$model" :class="{ error: v$.firstNumbers.$invalid && v$.firstNumbers.$dirty}" @blur="v$.firstNumbers.$touch" placeholder="Первые 6 цифр" />
-          <div class="input-errors" v-if="v$.firstNumbers.required.$invalid && v$.firstNumbers.$dirty">
-            <div class="error-msg">{{ v$.firstNumbers.required.$message }}</div>
-          </div>
-          <div class="input-errors" v-else-if="v$.firstNumbers.numeric.$invalid && v$.firstNumbers.$dirty">
-            <div class="error-msg">{{ v$.firstNumbers.numeric.$message }}</div>
-          </div>
-          <div class="input-errors" v-else-if="v$.firstNumbers.minLength.$invalid && v$.firstNumbers.$dirty">
-            <div class="error-msg">{{ v$.firstNumbers.minLength.$message }}</div>
-          </div>
-          <input type="text" v-model="v$.lastNumbers.$model" :class="{ error: v$.lastNumbers.$invalid && v$.lastNumbers.$dirty}" @blur="v$.lastNumbers.$touch" placeholder="Последние 4 цифры"/>
-          <div class="input-errors" v-if="v$.lastNumbers.required.$invalid && v$.lastNumbers.$dirty">
-            <div class="error-msg">{{ v$.lastNumbers.required.$message }}</div>
-          </div>
-          <div class="input-errors" v-else-if="v$.lastNumbers.numeric.$invalid && v$.lastNumbers.$dirty">
-            <div class="error-msg">{{ v$.lastNumbers.numeric.$message }}</div>
-          </div>
-          <div class="input-errors" v-else-if="v$.lastNumbers.minLength.$invalid && v$.lastNumbers.$dirty">
-            <div class="error-msg">{{ v$.lastNumbers.minLength.$message }}</div>
-          </div>
-          <input type="text" v-model="v$.sum.$model" :class="{ error: v$.sum.$invalid && v$.sum.$dirty}" @blur="v$.sum.$touch" placeholder="Сумма"/>
-          <div class="input-errors" v-if="v$.sum.required.$invalid && v$.sum.$dirty">
-            <div class="error-msg">{{ v$.sum.required.$message }}</div>
-          </div>
-          <div class="input-errors" v-else-if="v$.sum.numeric.$invalid && v$.sum.$dirty">
-            <div class="error-msg">{{ v$.sum.numeric.$message }}</div>
-          </div>
-          <v-date-picker v-model="date">
-            <template v-slot="{ inputValue, inputEvents }">
-              <input
-                class="px-2 py-1 border rounded focus:outline-none focus:border-blue-300"
-                :value="inputValue"
-                v-on="inputEvents"
-              />
-            </template>
-          </v-date-picker>  
-          <button type="submit">Найти автоплатеж</button>
-        </form>
+        <h1 class="modal__title">{{titleMap[id]}}</h1>
+        <div class="modal__scroll">
+          <form action="" method="" @submit.prevent="submit($event)" class="form" >
+            <div class="form__label">Введите номер карты</div>
+            <div class="form__row">
+              <div class="form__field">
+                <div class="input">
+                  <input type="text" class="input__field" v-model="v$.firstNumbers.$model" :class="{ error: v$.firstNumbers.$invalid && v$.firstNumbers.$dirty}" @blur="v$.firstNumbers.$touch" placeholder="Первые 6 цифр" />
+                  <div class="input__error" v-if="v$.firstNumbers.required.$invalid && v$.firstNumbers.$dirty">{{ v$.firstNumbers.required.$message }}</div>
+                  <div class="input__error" v-else-if="v$.firstNumbers.numeric.$invalid && v$.firstNumbers.$dirty">{{ v$.firstNumbers.numeric.$message }}</div>
+                  <div class="input__error" v-else-if="v$.firstNumbers.minLength.$invalid && v$.firstNumbers.$dirty">{{ v$.firstNumbers.minLength.$message }}</div>
+                </div>
+              </div>
+              <div class="form__field">
+                <div class="input">
+                  <input type="text" class="input__field" v-model="v$.lastNumbers.$model" :class="{ error: v$.lastNumbers.$invalid && v$.lastNumbers.$dirty}" @blur="v$.lastNumbers.$touch" placeholder="Последние 4 цифры"/>
+                  <div class="input__error" v-if="v$.lastNumbers.required.$invalid && v$.lastNumbers.$dirty">{{ v$.lastNumbers.required.$message }}</div>
+                  <div class="input__error" v-else-if="v$.lastNumbers.numeric.$invalid && v$.lastNumbers.$dirty">{{ v$.lastNumbers.numeric.$message }}</div>
+                  <div class="input__error" v-else-if="v$.lastNumbers.minLength.$invalid && v$.lastNumbers.$dirty">{{ v$.lastNumbers.minLength.$message }}</div>
+                </div>
+              </div>
+            </div>
+            <div class="form__label">Введите сумму автоплатежа</div>
+            <div class="form__row form__row--help">
+              <div class="form__field">
+                <div class="input input--sum">
+                  <input type="text" class="input__field" v-model="v$.sum.$model" :class="{ error: v$.sum.$invalid && v$.sum.$dirty}" @blur="v$.sum.$touch" placeholder="Сумма"/>
+                  <div class="input__error" v-if="v$.sum.required.$invalid && v$.sum.$dirty">{{ v$.sum.required.$message }}</div>
+                  <div class="input__error" v-else-if="v$.sum.numeric.$invalid && v$.sum.$dirty">{{ v$.sum.numeric.$message }}</div>
+                </div>
+              </div>
+              <button type="button" class="form__help">?</button>
+            </div>
+            <!--          <div class="form__field">-->
+            <!--            <v-date-picker v-model="date">-->
+            <!--              <template v-slot="{ inputValue, inputEvents }">-->
+            <!--                <div class="input input&#45;&#45;date">-->
+            <!--                  <input-->
+            <!--                    class="input__field"-->
+            <!--                    :value="inputValue"-->
+            <!--                    v-on="inputEvents"-->
+            <!--                  />-->
+            <!--                </div>-->
+            <!--              </template>-->
+            <!--            </v-date-picker>-->
+            <!--          </div>-->
+
+            <button type="submit" class="form__submit">Найти автоплатеж</button>
+            <div class="form__alert">
+              <app-alert type="success"></app-alert>
+            </div>
+            <app-result-box></app-result-box>
+            <app-result-box></app-result-box>
+          </form>
+        </div>
       </div>
     </article>
   `
