0183cea38c01a0a4cdb1aed0d9030515e11b6cd5
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-05-19 10:35:06 +1000

    Buyer faq's content added

--- src/assets/js/vue/app-faq.js
+++ src/assets/js/vue/app-faq.js
@@ -40,8 +40,8 @@ export default  {
       <div class="faq__section" v-for="section in tab">
         <h3 class="faq__title">{{section.title}}</h3>
         <div class="faq__item" v-for="item in section.items" @click="setActiveItem(item)" :key="item">
-          <b>{{item.question}}</b>
-          <p v-if="activeItem === item">{{item.answer || 'Ответ'}}</p>
+          <b v-html="item.question"></b>
+          <p v-if="activeItem === item" v-html="item.answer"></p>
           <svg  class="icon" width="48" height="38">
             <use xlink:href="assets/img/symbol/sprite.svg#caret" />
           </svg>
