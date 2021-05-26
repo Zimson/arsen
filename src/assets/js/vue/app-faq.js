77e8a8f2912b423a297a2813823926fe1ba7fc2a
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-05-27 05:24:37 +1000

    Styles for faq added

--- src/assets/js/vue/app-faq.js
+++ src/assets/js/vue/app-faq.js
@@ -34,22 +34,29 @@ export default  {
       })
     }
   },
+  computed: {
+    caret() {
+      return `assets/img/symbol/sprite.svg#caret-${this.activeItem ? 'up' : 'down'}`;
+    }
+  },
   template: `
     <app-tabs-nav :items="setTabsName(Object.keys(data))" v-on:activeTab="setActiveTab($event)"></app-tabs-nav>
     <div v-for="(tab, key, idx) in data" class="faq" :key="key" :style="{display: idx === activeTab ? 'block' : 'none'}">
       <div class="faq__section" v-for="section in tab">
-        <h3 class="faq__title">{{section.title}}</h3>
-        <div class="faq__item" v-for="item in section.items" @click="setActiveItem(item)" :key="item">
-          <b v-html="item.question"></b>
+        <h3 class="faq__title" v-html="section.title"></h3>
+        <div class="faq__item" v-for="item in section.items" :class="{'faq__item--active': activeItem === item}" @click="setActiveItem(item)" :key="item">
+          <div class="faq__item-header">
+            <b v-html="item.question"></b>
+<!--            <svg  class="icon" width="48" height="38">-->
+<!--              <use :xlink:href="caret" />-->
+<!--            </svg>-->
+          </div>
           <template v-if="Array.isArray(item.answer)">
-            <p v-for="text in item.answer" v-if="activeItem === item" v-html="text"></p>
+            <p v-for="text in item.answer" v-if="activeItem === item" v-html="text" :key="text"></p>
           </template>
           <template v-else>
             <p v-if="activeItem === item" v-html="item.answer"></p> 
           </template>
-          <svg  class="icon" width="48" height="38">
-            <use xlink:href="assets/img/symbol/sprite.svg#caret" />
-          </svg>
         </div>
       </div>
     </div>
