0309ddc1af412ded7a7a9142edf45cb288a96e03
Anton Mayatskiy <work.mayatskiy@gmail.com>
2022-03-01 07:11:32 +1000

    slider fixed

--- src/assets/js/modules/Slider/index.js
+++ src/assets/js/modules/Slider/index.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
         //     spaceBetween: 20
         //   }
         // }
-      }, turnOffOnBreakPoint: 1060});
+      }, turnOffOnBreakPoint: 1045});
   }
 });
 
