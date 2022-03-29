7d490c5a74cfd14341f9da3f2abcc6816be5d836
Anton Mayatskiy <work.mayatskiy@gmail.com>
2022-03-29 17:47:22 +1000

    hot fix

--- src/helpers/social.js
+++ src/helpers/social.js
@@ -20,7 +20,7 @@ module.exports = function(options) {
     <div class="${cssClass}">
         <span class="social__label">Мы в соцсетях</span>
         <a href="https://www.instagram.com/arsenal_pay/" class="social__item" target="_blank">
-            <img src="${root}assets/img/svg/instagram.svg" width="48" height="38" alt="Instagram">
+            <img src="${root}assets/img/svg/instagram.svg" width="48" height="38" alt="Instagram*">
         </a>
         <a href="tg://resolve?domain=arsenalpay" class="social__item">
             <img src="${root}assets/img/svg/telegram.svg" width="48" height="38" alt="Telegram">
